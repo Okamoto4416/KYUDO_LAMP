b0a40ad0883c74b96a0ffd863764949d45c14c56
tazmain <21t2117f@shinshu-u.ac.jp>
2026-05-26 02:53:34 +0900

    リモコン側とランプ側を統合して管理できるような構成にした。試行 ランプ側はmain_lamp リモコン側はmain_remote にかいて環境をかえてビルド、書き込みすればいいと思う。

---
+++ src/lamp/main_lamp.cpp
@@ -0,0 +1,104 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+const char* ssid     = "el2g-6cec04";
+const char* password = "402fng1001";
+
+IPAddress local_IP(192, 168, 0, 50);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+const int udpPort = 12345;
+
+WiFiUDP udp;
+char incomingPacket[255];
+
+// ピン設定
+const int buttonPin = 26;
+const int lampPin   = 14;
+
+// 状態
+volatile bool lampState = false;  // ON/OFF状態
+volatile bool blinkState = false; // 点滅用
+
+bool prevButton = false;
+
+// タイマ
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+// 割り込み処理（0.5秒ごと）
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  if (lampState) {
+    blinkState = !blinkState;
+    digitalWrite(lampPin, blinkState);
+  }
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(lampPin, OUTPUT);
+
+  //WiFi
+  WiFi.config(local_IP, gateway, subnet);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Failed");
+    delay(500);
+  }
+
+  udp.begin(udpPort);
+
+  // タイマ設定（80MHz / 80 = 1MHz → 1μs）
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 500000, true); // 0.5秒
+  timerAlarmEnable(timer);
+
+  Serial.println("device_OUT ready");
+}
+
+void toggleLamp() {
+  portENTER_CRITICAL(&timerMux);
+  lampState = !lampState;
+
+  if (!lampState) {
+    digitalWrite(lampPin, LOW);
+    blinkState = false;
+  }
+
+  portEXIT_CRITICAL(&timerMux);
+
+  Serial.print("Lamp: ");
+  Serial.println(lampState ? "ON" : "OFF");
+}
+
+void loop() {
+  // ボタン監視
+  bool currentButton = !digitalRead(buttonPin);
+  if (currentButton && !prevButton) {
+    Serial.print("button pushed ");
+    toggleLamp();
+  }
+  prevButton = currentButton;
+
+  // UDP受信
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    int len = udp.read(incomingPacket, 255);
+    if (len > 0) incomingPacket[len] = '\0';
+
+    Serial.print("Received: ");
+    Serial.println(incomingPacket);
+
+    if (strcmp(incomingPacket, "TRUE") == 0) {
+      toggleLamp();
+    }
+  }
+
+  delay(10);
+}
