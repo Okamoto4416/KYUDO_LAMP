8c100a6da5c8280523c36e187a839b621824608b
tazmain <21t2117f@shinshu-u.ac.jp>
2026-06-09 11:55:52 +0900

    チャタリング対策したデジタル読み取りのクラスの実装 PinEffects.hpp DebouncedDigitalRead GageDigitalRead

--- include/PinEffects.hpp
+++ include/PinEffects.hpp
@@ -11,10 +11,10 @@ L->H->L->H->L->H->L->L->
 */
 class PatternBlinker8bit
 {
-    const unsigned patternPeriodMs;  // 周期default:1s
-    uint8_t patternIndex{0}; // パターンの左から何番目のをみるか。0-7をとる
-    uint8_t pattern{0};      // 点滅パターンを01で表す
-    unsigned long nextChangeTimeMs;  // 次に切り替える時刻[ms]
+    const unsigned patternPeriodMs; // 周期default:1s
+    uint8_t patternIndex{0};        // パターンの左から何番目のをみるか。0-7をとる
+    uint8_t pattern{0};             // 点滅パターンを01で表す
+    unsigned long nextChangeTimeMs; // 次に切り替える時刻[ms]
     const uint8_t pin;
 
 public:
@@ -59,7 +59,6 @@ public:
     }
 };
 
-
 /**
  * パルスを出力する
  */
@@ -73,10 +72,10 @@ class PulseOutput
     bool isPulse = false; // パルス出力中か
 
 public:
-/** 
- * 出力ピンの設定とデフォルトのパルスの長さを設定する
- * pinは pinMode(pin,OUTOUT)しておく必要がある 
- * */
+    /**
+     * 出力ピンの設定とデフォルトのパルスの長さを設定する
+     * pinは pinMode(pin,OUTOUT)しておく必要がある
+     * */
     PulseOutput(uint8_t pin, unsigned pulseLengthMs = 10)
         : pin(pin),
           pulseLengthMs(pulseLengthMs) {}
@@ -115,5 +114,154 @@ public:
             idleLevel = LOW;
             pulseLevel = HIGH;
         }
+    }
+};
+
+/**
+ * チャタリング対策したデジタル読み取りを提供する
+ * ゲージ式
+ *
+ * gageCapacity : ゲージの容量[μs]
+ * thLow        : LOWに切り替わる閾値
+ * thHigh       : HIGHに切り替わる閾値
+ *
+ * 仕組み：
+ * 読み取り時
+ * HIGHの時ゲージをためる
+ * LOWの時ゲージを減らす
+ *
+ * ゲージがthHighを超えたときHIGHと判定する
+ * ゲージがthLowを下回った時LOWと判定する
+ *
+ * 使い方：
+ * update関数をloopで回しまくる。
+ * 読み取りたいときはread()する
+ */
+template <unsigned long gageCapacity = 10 * 1000,
+          unsigned long thLow = gageCapacity / 3,
+          unsigned long thHigh = gageCapacity / 3 * 2>
+class GageDigitalRead
+{
+    const uint8_t pin;
+    bool isHigh = false;
+    unsigned long gage = 0; // ゲージ 読み取り結果がHIGHだったら溜まり、LOWだったら減っていく
+    unsigned long preMicros;
+
+public:
+    Button(uint8_t pin, bool isHigh = false, unsigned gage = 0)
+        : pin(pin), isHigh(isHigh), gage(gage), preMicros(micros()) {}
+
+    // 呼び出さなくてよい
+    void init()
+    {
+        preMicros = micros();
+    }
+    void update()
+    {
+        auto now = micros();
+        auto deltaGage = micros() - preMicros;
+        if (digitalRead(pin))
+        {
+            // HIGHだったら経過マイクロ秒だけゲージを増やす
+            gage += std::min(deltaGage, gageCapacity - gage);
+        }
+        else
+        {
+            // LOWだったら経過マイクロ秒だけゲージを減らす
+            gage -= std::min(deltaGage, gage);
+        }
+
+        // 以下はupdateで必要(read時ではいけない)
+        if (isHigh && gage < thLow)
+        {
+            isHigh = false;
+        }
+        else if (!isHigh && thHigh < gage)
+        {
+            isHigh = true;
+        }
+        preMicros = now;
+    }
+
+    // 判定結果読み取り
+    int read() const
+    {
+        if (isHigh)
+            return HIGH;
+        else
+            return LOW;
+    }
+};
+
+/**
+ * チャタリング対策したデジタル読み取りを提供する
+ * 履歴式(軽量)
+ *
+ * T            : 読み取り履歴を保存するのに使用する符号なし整数型
+ * interval_    : 読み取り周期[ms]
+ * n_           : 読み取り履歴の最後いくつを判定にするか
+ *
+ * 仕組み：
+ * interval [ms]ごとに読み取り、履歴に入れていく
+ * 下n桁が全部1 → HIGH
+ * 下n桁が全部0 → LOW
+ * それ以外 → 前回状態維持
+ *
+ * 使い方：
+ * update関数をloopで回しまくる。
+ * 読み取りたいときはread()する
+ */
+template <typename UINT = uint8_t, unsigned interval = 5, unsigned n = 4>
+class DebouncedDigitalRead
+{
+    static constexpr unsigned bitWidth = sizeof(UINT) * 8;
+    static constexpr UINT mask = ~(~UINT{0} << n); // 下n桁が1
+    static_assert(std::is_integral<UINT>::value &&
+                      std::is_unsigned<UINT>::value,
+                  "UINTは符号なし整数の必要がある。");
+    static_assert(interval > 0, "intervalは正の数である必要がある。");
+    static_assert(0 < n && n <= bitWidth, "n は 0より大きく、UINTのbitサイズ以下である必要がある。");
+
+    unsigned long nextMillis; // 次の判定時刻
+    UINT history{0};          // 読み取り結果履歴HIGHが読み取られたら1が立ちシフトしていく
+    const uint8_t pin;        // 読み取る対象のpin
+    bool state = LOW;
+
+public:
+    DebouncedDigitalRead(uint8_t pin, bool state = false)
+        : pin(pin), state(state) nextMillis(millis()) {}
+
+    void update()
+    {
+
+        if (millisReached(nextMillis))
+        {
+            // 次の時間のセット
+            nextMillis += interval;
+
+            // 履歴に入れる
+            history <<= 1;
+            if (digitalRead(pin))
+                history |= UINT{1};
+
+            // 判定
+            if ((history & mask) == mask)
+            {
+                state = HIGH;
+            }
+            else if ((history & mask) == 0)
+            {
+                state = LOW;
+            }
+        }
+    }
+
+    // 判定結果読み取り
+    int read() const
+    {
+        if (state)
+            return HIGH;
+        else
+            return LOW;
     }
 };
