57ee30dc9af59d1ca1fee83938b303a56aff69d1
tazmain <21t2117f@shinshu-u.ac.jp>
2026-06-14 10:32:04 +0900

    network関係ledをつけてみた

--- src/remote/main_remote.cpp
+++ src/remote/main_remote.cpp
@@ -3,20 +3,37 @@
 #include "routerAP_config.hpp"
 // #include "test_remote.hpp"
 #include "NetWorkMngr.hpp"
+#include "PinEffects.hpp"
 
-const int buttonPin = 21; // 好きなGPIO
+constexpr unsigned buttonPin = 21;      // ボタンのpin
+constexpr unsigned NWpacketLEDpin = 19; // ネットワーク関係LEDパケットが来たら一瞬光る
+PulseOutput<5> NWpacketLEDpulse(NWpacketLEDpin);
+constexpr unsigned NWstateLEDpin = 18; // ネットワーク関係LED 状態の表示
+PatternBlinker8bit NWstateLEDblink(NWstateLEDpin);
+
+// packet処理
+// NetworkMngr.initで登録したので、パケットが来たら呼ばれる
+void processPacket(JsonObjectConst rx_json)
+{
+    auto type = rx_json["type"].as<const char *>(); // 項目typeの値の取り出し
+
+    // パケットが来たのでパルス
+    NWpacketLEDpulse.trigger();
+}
 
 void setup()
 {
     Serial.begin(115200);
 
+    // ピンのセット
     pinMode(buttonPin, INPUT_PULLUP);
+    pinMode(NWpacketLEDpin, OUTPUT);
+    pinMode(NWstateLEDpin, OUTPUT);
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // WiFiの準備
     // 自分ip,相手ip,パケット処理する関数の登録
-    // 今回パケット処理しないのでNOfnを指定しておく
-    NetworkMngr.init(remote_ip, lamp_ip);
+    NetworkMngr.init(remote_ip, lamp_ip, processPacket, processPacket);
 
     Serial.println("device_IN ready");
 }
@@ -25,29 +42,66 @@ void loop()
 {
     // update//////////////////////////////////////////////////////////////////////
     NetworkMngr.update();
+    NWpacketLEDpulse.update();
+    NWstateLEDblink.update();
+
+    // NW状態LED
+    {
+        static auto prevState = NetworkMngr.get_state();
+        auto currentState = NetworkMngr.get_state();
+
+        if (prevState != currentState)
+        {
+            // 状態が変化したら
+
+            switch (currentState)
+            {
+            case NetworkMngr_t::State_t::off:
+                NWstateLEDblink.setPattern(0);
+                break;
+
+            case NetworkMngr_t::State_t::connectingAP:
+                NWstateLEDblink.setPattern(0b00001111);
+                break;
+
+            case NetworkMngr_t::State_t::discoveringPeer:
+            case NetworkMngr_t::State_t::unstable:
+            case NetworkMngr_t::State_t::connected:
+                NWstateLEDblink.setPattern(0b10100000);
+                break;
+
+            default:
+                NWstateLEDblink.setPattern(0b11111111);
+                break;
+            }
+            NWstateLEDblink.restart();
+        }
+    }
 
     // ボタン監視 10msごと
-    static unsigned long buttonNextTime = millis();
-    static bool prevButton = false;
-    if (millisReached(buttonNextTime))
     {
-
-        bool currentButton = !digitalRead(buttonPin);
-
-        // 押した瞬間だけ送る（エッジ検出）
-        if (currentButton && !prevButton)
+        static unsigned long buttonNextTime = millis();
+        static bool prevButton = false;
+        if (millisReached(buttonNextTime))
         {
-            //////////////////////////////////////////////////////////////////////////////////////////
-            // 送信するときはjsonに書き込んで送る
-            auto txjson = NetworkMngr.beginTxJson(); // 書き込むjsonを取得
-            txjson["type"] = "toggle";               // type:"toggle"を書き込み
-            NetworkMngr.sendTxJson();                // 送信
-            //////////////////////////////////////////////////////////////////////////////////////////
 
-            Serial.println("Sent TRUE");
+            bool currentButton = !digitalRead(buttonPin);
+
+            // 押した瞬間だけ送る（エッジ検出）
+            if (currentButton && !prevButton)
+            {
+                //////////////////////////////////////////////////////////////////////////////////////////
+                // 送信するときはjsonに書き込んで送る
+                auto txjson = NetworkMngr.beginTxJson(); // 書き込むjsonを取得
+                txjson["type"] = "toggle";               // type:"toggle"を書き込み
+                NetworkMngr.sendTxJson();                // 送信
+                //////////////////////////////////////////////////////////////////////////////////////////
+
+                Serial.println("Sent TRUE");
+            }
+
+            prevButton = currentButton;
+            buttonNextTime += 10; // delay(10);
         }
-
-        prevButton = currentButton;
-        buttonNextTime += 10; // delay(10);
     }
 }
