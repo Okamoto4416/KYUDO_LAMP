92d24c35fff6ffc2ef21cc2738238f7b18e2e4ca
tazmain <21t2117f@shinshu-u.ac.jp>
2026-08-02 20:41:51 +0900

    型依存の定数を1UとかでなくてUINT{1U}とかで作る。

--- include/PinEffects.hpp
+++ include/PinEffects.hpp
@@ -20,8 +20,8 @@ class PatternBlinker
     PatternType pattern{0};         // 点滅パターンを01で表す
     uint8_t patternIndex{0};        // パターンの左から何番目のをみるか。0-7をとる
 public:
-    const uint8_t pin;                                            // 出力対象pin
-    static constexpr PatternType TOP = 1U << (patternLength - 1); // 0b10000...0 最初のシンボルの部分が1
+    const uint8_t pin;                                                         // 出力対象pin
+    static constexpr PatternType TOP = PatternType{1U} << (patternLength - 1); // 0b10000...0 最初のシンボルの部分が1
 
 public:
     PatternBlinker(const PatternBlinker &) = delete;
@@ -225,8 +225,8 @@ template <typename UINT = uint8_t, unsigned interval = 5, unsigned n = 4>
 class DebouncedDigitalRead
 {
     static constexpr unsigned bitWidth = sizeof(UINT) * 8;
-    static constexpr UINT mask = (UINT{1} << n) - 1;          // 下n桁が1
-    static constexpr UINT curCondMask = 1U << (bitWidth - 1); // 最上位だけ1
+    static constexpr UINT mask = (UINT{1} << n) - 1;                // 下n桁が1
+    static constexpr UINT curCondMask = UINT{1U} << (bitWidth - 1); // 最上位だけ1
     static_assert(std::is_integral<UINT>::value &&
                       std::is_unsigned<UINT>::value,
                   "UINTは符号なし整数の必要がある。");
